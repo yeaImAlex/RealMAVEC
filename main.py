e4c2a2adb94991fc0a64b2fcedc81aef79814270
yeaImAlex <96880676+yeaImAlex@users.noreply.github.com>
2024-10-09 14:12:41 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,48 +1,41 @@
 import laneDetection as ld
-import Motor as motorM
-import Camera as C
-import BoomGateDetection as bgd  
-from keyboardM import Keyboard1
+import test as motorM
+from CameraT import Camera
+from KeyboardM import Keyboard1
 import cv2
 
-motor = motorM.Motor(12, 16, 27, 17, 21, 13, 26, 19, 18)
-kM = Keyboard1()
+# Initialize the motor and keyboard
+motor = motorM.Motor(12, 16, 27, 17, 21, 13, 26, 19, 18)  # Motor GPIO pin setup
+kM = Keyboard1()  # Initialize the keyboard input handler
+C = Camera()  # Initialize the camera
 
 if __name__ == '__main__':
+    print('Camera start')
     try:
-        pipeline = C.create_pipeline(fps=30)
-        device, videoQueue = C.start_camera(pipeline)
-
-        intialTrackBarVals = [98, 133, 0, 240]
+        intialTrackBarVals = [84, 182, 0, 303]
         ld.utils.initializeTrackbars(intialTrackBarVals)
 
         while True:
-            frame = C.show_video(videoQueue)
+            frame = C.show_video()  # Get the camera frame
             if frame is None:
+                print('Frame not shown')
                 break
 
-            # Detect the boom gate
-            processed_frame, boom_gate_detected = bgd.detect_red_gate(frame)
+            # Lane detection logic
+            curve = ld.getLaneCurve(frame, display=1)
+            print(curve)
 
-            if boom_gate_detected:
-                motor.stop()
+
+            if -0.02 <= curve <= 0.02:  
+                motor.move(15, 0)
             else:
-                curve = ld.getLaneCurve(frame, display=1)
-
-                if -0.05 <= curve <= 0.05:
-                    motor.move(30, 90)  
-                else:
-                    delay = 0.58
-                    curve *= delay
-                    motor.move(30, curve)  
-
-
-            cv2.imshow('Boom Gate Detection', processed_frame)
-
+                delay = 1
+                curve *= delay
+                motor.move(15, curve)
 
             if kM.getKey('q'):
                 break
 
     finally:
-        motor.cleanup()
-        cv2.destroyAllWindows()
+        motor.cleanup()  
+        cv2.destroyAllWindows()  
